5d7cd19da708f54b899f5c6cf824d19946f3591b
Rikuya Osawa <rikuyaosawa.jp@gmail.com>
2026-03-19 16:11:38 +0900

    Update GITHUB_URL in site-config to reflect new repository name

--- src/constants/site-config.ts
+++ src/constants/site-config.ts
@@ -1,3 +1,3 @@
 export const SITE_NAME = "PASCAL";
 export const DEFAULT_DESCRIPTION = "日本語圏最大のメンタルモデル辞書。意思決定を支える「思考の地図」を提供します。";
-export const GITHUB_URL = "https://github.com/rikuya-osawa/mental-model-atlas";
+export const GITHUB_URL = "https://github.com/rikuya-osawa/pascal";
